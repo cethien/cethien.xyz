2ffe72588a2ef57d0a94e6b38cd5231dd35b4d04
cethien <borislaw.sotnikow@gmx.de>
2023-07-16 18:11:28 +0200

    chore: moved into monorepo feat: basic layout

---
+++ apps/web/src/consts.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = 'Cethien';
+export const APP_DESCRIPTION = "cethien's personal stuff";
+
+export const HEADER_TITLE = 'cethien.xyz';
+export const GITHUB_LINK = 'https://github.com/cethien/cethien.xyz';
