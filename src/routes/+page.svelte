2a9f5bc351df5cd63f889217a494b672a27ef9dc
cethien <borislaw.sotnikow@gmx.de>
2024-10-04 10:44:52 +0200

    style: mobile layout, link colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
-<article class="prose dark:prose-invert xl:prose-xl">
+<article class="prose prose-xl dark:prose-invert sm:prose-lg">
 	<h1>Hi there!</h1>
 	<p>this is a website i made for no reason.</p>
 	<p>my links:</p>
 
 	<div
-		class="flex w-min items-stretch justify-center gap-4 text-center font-mono text-3xl font-semibold"
+		class="font-full flex w-full flex-col items-stretch justify-center gap-4 text-center font-mono text-3xl sm:w-min sm:flex-row"
 	>
 		<a
 			href="https://github.com/cethien"
 			target="_blank"
 			rel="noopener noreferrer"
-			class=" rounded-md bg-zinc-600 px-4 py-2 text-white no-underline hover:bg-sky-700"
+			class=" rounded-md bg-zinc-800 px-4 py-2 text-white no-underline hover:bg-zinc-600 dark:bg-zinc-600 hover:dark:bg-zinc-500"
 		>
 			<div class="flex items-center gap-2">
 				<span class="icon-[mdi--github]"></span>
@@ -22,7 +22,7 @@
 			href="https://linkedin.com/in/borislaw-sotnikow/"
 			target="_blank"
 			rel="noopener noreferrer"
-			class="rounded-md bg-sky-600 px-4 py-2 text-white no-underline hover:bg-sky-700 dark:bg-sky-600"
+			class="rounded-md bg-cyan-500 px-4 py-2 text-white no-underline hover:bg-cyan-400 dark:bg-cyan-600 hover:dark:bg-cyan-500"
 		>
 			<div class="flex items-center gap-2">
 				<span class="icon-[mdi--linkedin]"></span>
